d1146eaedd4241018af14dc11ea97d581da4fc47
cbird808 <cbird808@gmail.com>
2022-04-26 13:58:24 -0500

    uppdate script

--- data/RotenoneRovingCombined.R
+++ data/RotenoneRovingCombined.R
@@ -2,9 +2,20 @@
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# install.packages("tidyverse")
+# install.packages("readxl")
+
 library(tidyverse)
 library(readxl)
 
 data_roving <-
   read_excel("RotenoneRovingCombined.xlsx",
              sheet = "Roving")
+
+data_rotenone <-
+  read_excel("RotenoneRovingCombined.xlsx",
+             sheet = "Rotenone")
+
+data_combined <-
+  read_excel("RotenoneRovingCombined.xlsx",
+             sheet = "Combined")
