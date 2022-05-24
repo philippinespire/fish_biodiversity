4a10ab255c09c13fd2dcf72f7fb6d72e17d87cd4
cbird808 <cbird808@gmail.com>
2022-05-24 14:23:18 -0500

    rr ceb meeting

--- scripts/wrangleStationData_SI.R
+++ scripts/wrangleStationData_SI.R
@@ -11,45 +11,69 @@ library(janitor)
 library(purrr)
 
 #### USER DEFINED VARIABLES ####
-metadataDir = "../SI/Collections_Data"
-metadataPattern = "*.csv"
+querydataDir = "../SI/Collections_Data"
+querydataPattern = "*.csv"
+gisDataFile = "../SI/Coordinates/Coordinate_Conversions.xlsx"
 
 
-#### READ IN METADATA ####
+#### READ IN COUNT DATA ####
 
-metadata <-
-  list.files(metadataDir,
-             metadataPattern,
+data_si <-
+  list.files(querydataDir,
+             querydataPattern,
              full.names = TRUE) %>%
   map(.,
       ~ read_csv(.x)) %>%
   bind_rows() %>%
   clean_names() %>%
   distinct(catalog_number_usnm, # if we don't do this, get 2 more records
-           .keep_all = TRUE)
+           .keep_all = TRUE) %>%
+  rename(prep_loc_count = preparation_details_preparation_location_count,
+         field_number = field_number_s,
+         collectors = collector_s) %>%
+  mutate(odu_station_code = str_replace(field_number,
+                                        " ",
+                                        "_"))
 
-metadata %>% 
-  select(field_number_s) %>%
-  unique() %>%
-  view()
-  
-metadata %>% 
-  filter(is.na(field_number_s)) %>%
-  view()
+#### READ IN GIS DATA ####
+data_si_gis <-
+  data_si %>%
+  left_join(data_gis,
+            by = "odu_station_code")
 
-metadata %>% 
-  select(field_number_s,
-         date_collected) %>%
-  distinct() %>%
-  arrange(date_collected,
-          field_number_s) %>%
-  view()
+data_gis <-
+  read_excel(gisDataFile) %>%
+  clean_names() %>%
+  select(odu_station_code:smithsonian_station_code,
+         starts_with("adjusted_"),
+         -starts_with("x"))
 
-metadata %>% 
-  select(field_number_s,
-         date_collected) %>%
-  distinct() %>%
-  arrange(field_number_s,
-          date_collected) %>%
-  view()
+#### JOIN DATA ####
 
+
+# #### DATA CHECKING ####
+# data_si %>% 
+#   select(field_number_s) %>%
+#   unique() %>%
+#   view()
+#   
+# data_si %>% 
+#   filter(is.na(field_number_s)) %>%
+#   view()
+# 
+# data_si %>% 
+#   select(field_number_s,
+#          date_collected) %>%
+#   distinct() %>%
+#   arrange(date_collected,
+#           field_number_s) %>%
+#   view()
+# 
+# data_si %>% 
+#   select(field_number_s,
+#          date_collected) %>%
+#   distinct() %>%
+#   arrange(field_number_s,
+#           date_collected) %>%
+#   view()
+# 
