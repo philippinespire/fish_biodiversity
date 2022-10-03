16baa7c6dd66437f5a2d58c3eeea48aa2062f487
RebeccaRuiz <mrebeccaruiz@gmail.com>
2022-10-03 13:36:28 -0500

    full scripts

--- scripts/full_script.R
+++ scripts/full_script.R
@@ -3,6 +3,21 @@
 # install.packages("maps")
 # install.packages("viridis")
 # install.packages("geosphere")
+# devtools::install_github("singmann/afex@master")
+# devtools::install_github("eclarke/ggbeeswarm")
+# install.packages("multcomp")
+# install.packages("multcompView")
+# install.packages("performance")
+# install.packages("optimx")
+# install.packages("effects")
+# install.packages("ggeffects")
+# install.packages("afex")
+# install.packages("ggbeeswarm")
+# install.packages("prediction")
+# install.packages("fitdistrplus")
+# install.packages("rlang")
+# install.packages("emmeans")
+
 library(ggbiplot)
 library(geosphere)
 library(readxl)
@@ -16,5 +31,34 @@ library(viridis)
 library(vegan)
 library(ggvegan)
 library(remotes)
+library(devtools)
+library(fitdistrplus)
+library(emmeans)
+library(multcomp)
+library(multcompView)
+library(ggeffects)
+library(rlang)
+library(afex)
+library(ggbeeswarm)
+library(performance)
+library(optimx)
+library(effects)
+library(prediction)
 library(tidyverse)
+
+
+#### USER DEFINED VARIABLES ####
+
+wrangle_si_data_path = "./wrangleStationData_SI.R"
+wrangle_su_si_data_path = "./wrangle_SU-SI_DuplicatesNewData.R"
+wrangle_cas_data_path = "./wrangle_cas_data.R"
+
+#### READ IN DATA ####
+source(wrangle_si_data_path)
+source(wrangle_su_si_data_path)
+source(wrangle_cas_data_path)
+source("distance_calculations.R")
+source("ordination_cas_su_si.R")
+source("wrangle_cas_si_su_data.R")
+source("EstimateR.R")
 
